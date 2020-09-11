8f14d401b483bf8969b6818ce22991efef0993f3
Kimli <jgoodsir1210@gmail.com>
2020-09-11 16:02:46 +1000

    retrieve data from API and store in redux

--- src/App.js
+++ src/App.js
@@ -3,56 +3,125 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    // In production, a more robust aynsc method should probably used. This will do for now.
+    fetch('https://reqres.in/api/users')
+      .then(response => response.json())
+      .then(data => {
+        // Store the resquested data in Redux
+        const tableData = data.data;
+        this.setState({ tableData })
+      })
+      .then(() => console.log(this.state.tableData));
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Counter />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+            </p>
+          <span>
+            <span>Learn </span>
+            <a
+              className="App-link"
+              href="https://reactjs.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              React
+              </a>
+            <span>, </span>
+            <a
+              className="App-link"
+              href="https://redux.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Redux
+              </a>
+            <span>, </span>
+            <a
+              className="App-link"
+              href="https://redux-toolkit.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Redux Toolkit
+              </a>
+              ,<span> and </span>
+            <a
+              className="App-link"
+              href="https://react-redux.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              React Redux
+              </a>
+          </span>
+        </header>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <Counter />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <span>
+//           <span>Learn </span>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             React
+//           </a>
+//           <span>, </span>
+//           <a
+//             className="App-link"
+//             href="https://redux.js.org/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Redux
+//           </a>
+//           <span>, </span>
+//           <a
+//             className="App-link"
+//             href="https://redux-toolkit.js.org/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Redux Toolkit
+//           </a>
+//           ,<span> and </span>
+//           <a
+//             className="App-link"
+//             href="https://react-redux.js.org/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             React Redux
+//           </a>
+//         </span>
+//       </header>
+//     </div>
+//   );
+// }
+
+
 
 export default App;
