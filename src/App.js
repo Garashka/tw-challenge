40f49fdc8300ae00a8ef4c18eff0728de9f97dea
Kimli <jgoodsir1210@gmail.com>
2020-09-11 22:27:06 +1000

    simple table displaying from redux info

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import DirectoryTable from './features/user-directory/DirectoryTable';
 import './App.css';
 
 class App extends React.Component {
-  componentDidMount() {
-    // In production, a more robust aynsc method should probably used. This will do for now.
-    fetch('https://reqres.in/api/users')
-      .then(response => response.json())
-      .then(data => {
-        // Store the resquested data in Redux
-        const tableData = data.data;
-        this.setState({ tableData })
-      })
-      .then(() => console.log(this.state.tableData));
-
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <DirectoryTable />
         </header>
       </div>
     );
--- src/features/user-directory/DirectoryTable.js
+++ src/features/user-directory/DirectoryTable.js
@@ -1,8 +1,23 @@
+import { Paper } from '@material-ui/core';
 import React from 'react';
 import { connect } from 'react-redux';
-import { setUsers } from './userSlice';
+import { setUsers } from './directorySlice';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import { compose } from '@reduxjs/toolkit';
 
-class UserTable extends React.Component {
+const styles = theme => ({
+  root: {
+    width: '100%',
+  },
+});
+
+class DirectoryTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = { tableData: [] };
@@ -15,8 +30,67 @@ class UserTable extends React.Component {
     fetch('https://reqres.in/api/users')
       .then(response => response.json())
       .then(data => {
-        // Store the resquested data in Redux
+        // Store the requested data in Redux
         setUsers(data);
       })
   }
+
+  render() {
+    const { classes, tableData } = this.props;
+
+    return (
+      <Paper className={classes.root}>
+        <TableContainer className={classes.table}>
+          <Table stickyHeader>
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>First Name</TableCell>
+                <TableCell>Surname</TableCell>
+                <TableCell>Email</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {tableData.map((row) => (
+                <Row row={row} />
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+    )
+  }
 }
+
+// Render row as a function object, to make it simpler to hide/show avatars later
+function Row(props) {
+  const { row } = props;
+  return (
+    <React.Fragment>
+      <TableRow key={row.id}>
+        <TableCell>{row.id}</TableCell>
+        <TableCell>{row.first_name}</TableCell>
+        <TableCell>{row.last_name}</TableCell>
+        <TableCell>{row.email}</TableCell>
+      </TableRow>
+    </React.Fragment>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    tableData: state.directory.users
+  };
+}
+
+const mapDispatchToProps = {
+  setUsers,
+}
+
+// Compose combines multiple higher order components
+export default compose(
+  connect(mapStateToProps,
+    mapDispatchToProps
+  ),
+  withStyles(styles),
+)(DirectoryTable);
