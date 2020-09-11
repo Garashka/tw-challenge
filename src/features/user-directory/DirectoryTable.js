c3cf4c1825661734400123761ec83fe377839c3f
Kimli <jgoodsir1210@gmail.com>
2020-09-11 22:35:58 +1000

    hide/show user avatars

--- src/features/user-directory/DirectoryTable.js
+++ src/features/user-directory/DirectoryTable.js
@@ -3,13 +3,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { setUsers } from './directorySlice';
 import { withStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Collapse from '@material-ui/core/Collapse';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
+import TableFooter from '@material-ui/core/TableFooter';
 import TableRow from '@material-ui/core/TableRow';
+import TablePagination from '@material-ui/core/TablePagination';
 import { compose } from '@reduxjs/toolkit';
+import IconButton from '@material-ui/core/IconButton';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import { TableSortLabel } from '@material-ui/core';
 
 const styles = theme => ({
   root: {
@@ -44,6 +52,7 @@ class DirectoryTable extends React.Component {
           <Table stickyHeader>
             <TableHead>
               <TableRow>
+                <TableCell></TableCell>
                 <TableCell>ID</TableCell>
                 <TableCell>First Name</TableCell>
                 <TableCell>Surname</TableCell>
@@ -65,13 +74,30 @@ class DirectoryTable extends React.Component {
 // Render row as a function object, to make it simpler to hide/show avatars later
 function Row(props) {
   const { row } = props;
+  const [open, setOpen] = React.useState(false);
   return (
     <React.Fragment>
       <TableRow key={row.id}>
+        <TableCell>
+          <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
         <TableCell>{row.id}</TableCell>
         <TableCell>{row.first_name}</TableCell>
         <TableCell>{row.last_name}</TableCell>
         <TableCell>{row.email}</TableCell>
+      </TableRow>
+      {/* Collapsible user avatar */}
+      <TableRow>
+        {/* TODO: Display loading symbol */}
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto">
+            <Box margin={1}>
+              <img src={row.avatar} alt="User Avatar" />
+            </Box>
+          </Collapse>
+        </TableCell>
       </TableRow>
     </React.Fragment>
   )
